c02e9c4fd0e405da7708d14ee51e00b9b186c2bb
ahmedelgizawy <elgizawy.ahmed.21@gmail.com>
2024-08-20 13:46:46 +0200

    Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ProductsComponent } from './products/products.component';
 import { AddProductComponent } from './add-product/add-product.component';
+import { NewComponentTestComponent } from './new-component-test/new-component-test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
-    AddProductComponent
+    AddProductComponent,
+    NewComponentTestComponent
   ],
   imports: [
     BrowserModule,
